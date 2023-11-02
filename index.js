1ad67cc6107a9fd1eaf3a1c5e957702a3591bbe3
Ibrahim <ibkhalil5425@gmail.com>
2023-11-02 22:53:29 +0600

    update index.js file

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').renderFile);
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(middleware)
 app.use(router) 
 
